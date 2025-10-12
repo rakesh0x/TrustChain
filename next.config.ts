98e45f006cbffae498ae29b663305d0d25d05441
rakesh <rakeshcodes2005@gmail.com>
2025-10-12 12:22:53 +0530

    build-fixes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    // appDir must be enabled for /app routes
     appDir: true,
-    // Disable Turbopack
-    turbo: false,
   },
 };
 
