b6b3874e8a34e4b77b914d7d257e352bd4235b3a
rakesh <rakeshcodes2005@gmail.com>
2025-10-12 12:49:04 +0530

    sidebar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,46 +1,68 @@
 "use client";
+import React, { useState } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "../../components/ui/sidebar"
+import {
+  Home,
+  PiggyBank,
+  Settings,
+  Plus,
+  Contact2,
+  Search,
+} from "lucide-react";
 
-import { useState } from "react";
+export default  function SidebarDemo() {
+  const links = [
+    {
+      label: "Home",
+      href: "/home",
+      icon: (
+        <Home className="h-5 w-5 flex-shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "My Funds",
+      href: "/my-funds",
+      icon: (
+        <PiggyBank className="h-5 w-5 flex-shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Discover",
+      href: "/discover",
+      icon: (
+        <Search className="h-5 w-5 flex-shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Create Fund",
+      href: "/create",
+      icon: (
+        <Plus className="h-5 w-5 flex-shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+      icon: (
+        <Contact2 className="h-5 w-5 flex-shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+  ];
 
-export default function Home() {
-  const [file, setFile] = useState<File>();
-  const [url, setUrl] = useState("");
-  const [uploading, setUploading] = useState(false);
-
-  const uploadFile = async () => {
-    try {
-      if (!file) {
-        alert("No file selected");
-        return;
-      }
-
-      setUploading(true);
-      const data = new FormData();
-      data.set("file", file);
-      const uploadRequest = await fetch("/api/files", {
-        method: "POST",
-        body: data,
-      });
-      const signedUrl = await uploadRequest.json();
-      setUrl(signedUrl);
-      setUploading(false);
-    } catch (e) {
-      console.log(e);
-      setUploading(false);
-      alert("Trouble uploading file");
-    }
-  };
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFile(e.target?.files?.[0]);
-  };
+  const [open, setOpen] = useState(false);
 
   return (
-        <main className="w-full min-h-screen m-auto flex flex-col justify-center items-center">
-            <input type="file" onChange={handleChange} />
-            <button type="button" disabled={uploading} onClick={uploadFile}>
-                {uploading ? "Uploading..." : "Upload"}
-            </button>
-            {url && <img src={url} alt="Image from Pinata" />}
-        </main>
+    <Sidebar open={open} setOpen={setOpen} animate={false}>
+      <SidebarBody>
+        <div className="mt-[-26px] flex w-full flex-col gap-4">
+          <div className="px-4 py-2"></div>
+          <div className={open ? "pl-[12px]" : "pl-[5px]"}>
+            {links.map((link, idx) => (
+              <SidebarLink key={idx} link={link} />
+            ))}
+          </div>
+        </div>
+      </SidebarBody>
+    </Sidebar>
   );
 }
